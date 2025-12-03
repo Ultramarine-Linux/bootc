1a6b94456b313f72b5c0936439dbd3aa62a66127
Cappy Ishihara <cappy@cappuchino.xyz>
2025-12-03 19:35:58 +0700

    use hack

---
+++ tier2/nvidia/nvidia.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# HACK: Copied from 
+# https://github.com/zirconium-dev/zirconium/blob/main/build_files/03-nvidia.sh
+
+# TOOD: Probably use terra-nvidia repo instead
+
+set -xeuo pipefail
+
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
+dnf config-manager setopt fedora-nvidia.enabled=0
+sed -i '/^enabled=/a\priority=90' /etc/yum.repos.d/fedora-nvidia.repo
+
+dnf -y install --enablerepo=fedora-nvidia akmod-nvidia
+mkdir -p /var/tmp # for akmods
+chmod 1777 /var/tmp
+sed -i "s/^MODULE_VARIANT=.*/MODULE_VARIANT=kernel-open/" /etc/nvidia/kernel.conf
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+cat /var/cache/akmods/nvidia/*.failed.log || true
+
+dnf -y install --enablerepo=fedora-nvidia \
+    nvidia-driver-cuda libnvidia-fbc libva-nvidia-driver nvidia-driver nvidia-modprobe nvidia-persistenced nvidia-settings
+
+dnf config-manager addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
+dnf config-manager setopt nvidia-container-toolkit.enabled=0
+dnf config-manager setopt nvidia-container-toolkit.gpgcheck=1
+
+dnf -y install --enablerepo=nvidia-container-toolkit \
+    nvidia-container-toolkit
+
+curl --retry 3 -L https://raw.githubusercontent.com/NVIDIA/dgx-selinux/master/bin/RHEL9/nvidia-container.pp -o nvidia-container.pp
+semodule -i nvidia-container.pp
+rm -f nvidia-container.pp
+
+tee /usr/lib/modprobe.d/00-nouveau-blacklist.conf <<'EOF'
+blacklist nouveau
+options nouveau modeset=0
+EOF
+
+tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<'EOF'
+kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1"]
+EOF
+
+# Universal Blue specific Initramfs fixes
+mv /etc/modprobe.d/nvidia-modeset.conf /usr/lib/modprobe.d/nvidia-modeset.conf
+# we must force driver load to fix black screen on boot for nvidia desktops
+sed -i 's/omit_drivers/force_drivers/g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+# as we need forced load, also must pre-load intel/amd iGPU else chromium web browsers fail to use hardware acceleration
+sed -i 's/ nvidia / i915 amdgpu nvidia /g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+
+tee /usr/lib/systemd/system/nvctk-cdi.service <<'EOF'
+[Unit]
+Description=nvidia container toolkit CDI auto-generation
+ConditionFileIsExecutable=/usr/bin/nvidia-ctk
+After=local-fs.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable nvctk-cdi.service
+systemctl disable akmods-keygen@akmods-keygen.service
+systemctl mask akmods-keygen@akmods-keygen.service
+systemctl disable akmods-keygen.target
+systemctl mask akmods-keygen.target
