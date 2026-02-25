f41b5d5cf7d1b9acdaa7290a7e81da1bb12b158e
Cappy Ishihara <cappy@cappuchino.xyz>
2026-02-26 01:02:33 +0700

    add bootc dracut module

--- base/common.sh
+++ base/common.sh
@@ -12,7 +12,7 @@ dracut_rebuild() {
     KERNEL_VERSION="$(get_kernel_version)"
     export DRACUT_NO_XATTR=1
     echo "Rebuilding initramfs for kernel version: $KERNEL_VERSION"
-    dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+    dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree --add bootc -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
     chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
 
     setfattr -n user.component -v "initramfs" "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
