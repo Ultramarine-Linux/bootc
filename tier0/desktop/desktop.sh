f49b0eb858c4f9fba19681cc3aa96cf5ca4df0d2
Cappy Ishihara <cappy@cappuchino.xyz>
2026-06-06 08:18:27 +0700

    Remove PackageKit-bootc, use plasma-discover-rpm-ostree instead
    While we are on bootc, ironically the rpm-ostree backend works fine here
    as long as we lock rpm-ostree derivations.
    
    Also fixes Plasma crashing on startup due to PackageKit shenanigans

--- tier0/desktop/desktop.sh
+++ tier0/desktop/desktop.sh
@@ -10,13 +10,7 @@ systemctl --global enable xdg-user-dirs.service || true
 systemctl disable sssd-kcm.service sssd-kcm.socket || true
 systemctl enable flatpak-add-flathub-repos.service
 
-# edit /etc/PacageKit/PackageKit.conf to use bootc backend
-sed -i 's/#DefaultBackend=.*/DefaultBackend=bootc/' /etc/PackageKit/PackageKit.conf
-
 setfattr -n user.component -v "packagekit-config" /etc/PackageKit/PackageKit.conf
-
-# Remove DNF backend from PackageKit for good measure, this will be re-installed on `standard`
-rm -f /usr/lib64/packagekit-backend/libpk_backend_dnf.so
 
 echo ntsync | tee /usr/lib/modules-load.d/ntsync.conf
 
